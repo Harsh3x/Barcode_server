4b70378bd552e8b198d11758c2b3309a5d6dd531
Harsh3x <harshdubeyphone@gmail.com>
2025-01-02 13:43:30 +0530

    fi

--- build.sh
+++ build.sh
@@ -2,13 +2,13 @@
 
 # Update the package list
 apt-get update
+apt-get update
 
 # Install zbar tools and library
-apt install libzbar0
 apt-get install -y zbar-tools libzbar0
 
 # Install additional system dependencies that pyzbar might require
 apt-get install -y gcc python3-dev
 
-# Install Python dependencies
+# Install Python dependencies from requirements.txt
 pip install -r requirements.txt
